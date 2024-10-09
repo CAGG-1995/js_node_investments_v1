bb5e323973192b61c6d45a1abd883a1349d040fc
CAGG-1995 <cristian.ale.gu.gu@gmail.com>
2024-10-08 19:38:53 -0600

    working in method signup user
    1 - add the functions decodePassword, createdUUID, encryptPassword

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,13 +1,20 @@
+const { assembleResponse, decodePassword, createdUUID, encryptPassword } = require("../helpers/functions");
 
 const signUp = async (request, response) => {
 
     try {
 
-        return response.status(200).json(assembleResponse(true, 500, 'OK', { session: true } ));
+        const { email, password } = request.body;
+        const UUID = createdUUID();
+        const DECODED_PASSWORD = decodePassword(password);
+        const ENCRYPT_PASSWORD = await encryptPassword(DECODED_PASSWORD);
+
+
+        return response.status(201).json(assembleResponse(false, 'OK', { email, password, UUID, DECODED_PASSWORD, ENCRYPT_PASSWORD } ));
         
     } catch (error) {
 
-        return response.status(500).json(assembleResponse(true, 500, error.message, { errors:  stack } ));
+        return response.status(500).json(assembleResponse(true, error.message, { errors:  error.stack } ));
     }
 }
 
--- src/helpers/functions.js
+++ src/helpers/functions.js
@@ -1,3 +1,41 @@
-const assembleResponse = (error = false, msg = "Success", body = {}) => { error, code, msg, body }
+require("dotenv").config();
+
+const fs = require("fs");
+const path = require("path");
+const bcrypt = require("bcrypt");
+const jsonwebtoken = require("jsonwebtoken");
+
+const JWT_SECRET = process.env.JWT_SECRET;
+
+const assembleResponse = (error = false, msg = "Success", body = {}) => { return { error, msg, body }};
 
 //const destructuring = (error) => { return { message, name, stack } = error };
+
+const createdUUID = () => {
+
+    let dt = new Date().getTime();
+
+    const uuid = "xxxxxxxxxxxx4xxxyxxxxxxxxxxxxxxx".replace(/[xy]/g, (c) => {
+        let r = (dt + Math.random() * 16) % 16 | 0;
+        dt = Math.floor(dt / 16);
+        return (c == "x" ? r : (r & 0x3 | 0x8)).toString(16);
+    });
+    
+    return uuid;
+}
+
+const decodePassword = (password) => { return Buffer.from(password, "base64").toString("utf8"); }
+
+const encryptPassword = async (string) => {
+
+    const SALT = bcrypt.genSaltSync(10);
+
+    return await bcrypt.hash(string, SALT);
+};
+
+module.exports = {
+    assembleResponse,
+    createdUUID,
+    decodePassword,
+    encryptPassword
+}
